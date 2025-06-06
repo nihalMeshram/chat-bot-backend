4af1fcad2b6e6212e9a7a011500118ba446867f7
Nihal Meshram <meshramnihal24@gmail.com>
2025-06-07 02:15:14 +0530

    feat: added config module

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true })
   );
-  await app.listen(3000, '0.0.0.0');
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  const host = configService.get<string>('HOST') || '0.0.0.0';
+  await app.listen(port, host);
+  console.log(`Server running at http://${host}:${port}`);
 }
 bootstrap();
