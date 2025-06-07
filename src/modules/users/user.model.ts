992bff1df065b730aebe8edc2424815b7625b3c2
Nihal Meshram <meshramnihal24@gmail.com>
2025-06-07 21:34:34 +0530

    feat: implemented user model

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,7 +1,41 @@
 import {
   Table,
+  Column,
   Model,
+  PrimaryKey,
+  Default,
+  DataType,
+  BeforeCreate,
+  BeforeUpdate,
 } from 'sequelize-typescript';
+import * as bcrypt from 'bcrypt';
+import { UserRole } from './types/user-role.type';
 
 @Table({ timestamps: true, paranoid: true, tableName: 'users' })
-export class User extends Model<User> {}
+export class User extends Model<User> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @Column
+  fullName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column
+  password: string;
+
+  @Column(DataType.ENUM(...Object.values(UserRole)))
+  role: UserRole;
+
+  @BeforeCreate
+  @BeforeUpdate
+  static async hashPassword(instance: User) {
+    if (instance.changed('password')) {
+      const salt = await bcrypt.genSalt(10);
+      instance.password = await bcrypt.hash(instance.password, salt);
+    }
+  }
+}
