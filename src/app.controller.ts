c15584f94e4b7e1b23ebca49d248e138cca1f1c3
Nihal Meshram <meshramnihal24@gmail.com>
2025-06-11 23:01:47 +0530

    chore: added api docs for heath check api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 /**
  * AppController handles basic application-level routes.
  * Primarily used for system health checks or root endpoints.
  */
+@ApiTags('App') // Tags the controller for grouping in Swagger UI
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
@@ -17,6 +19,8 @@ export class AppController {
    * @returns {string} Health status message (e.g., 'ok')
    */
   @Get('/health')
+  @ApiOperation({ summary: 'Check health', description: 'Returns the health status of the application.' })
+  @ApiResponse({ status: 200, description: 'Application is healthy.' })
   getHealth(): string {
     return this.appService.getHealth();
   }
