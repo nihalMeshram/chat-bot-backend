fad9499512a77e874407dccbd1977888cbbfbb60
Nihal Meshram <meshramnihal24@gmail.com>
2025-06-14 06:11:00 +0530

    feat: provide document status service

--- src/modules/documents/document.module.ts
+++ src/modules/documents/document.module.ts
@@ -3,10 +3,14 @@ import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Document } from './document.model';
+import { DocumentStatusService } from './document-status.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Document])],
-  providers: [DocumentService],
+  providers: [
+    DocumentService,
+    DocumentStatusService,
+  ],
   controllers: [DocumentController]
 })
 export class DocumentModule {}
